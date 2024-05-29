e2d607901e140bb3f66c0d4e448c1dc4a8deab75
Bill Sideris <bill88t@feline.gr>
2024-05-29 03:05:01 +0300

    copyfile

--- files/strap.py
+++ files/strap.py
@@ -3,16 +3,16 @@ try:
 except FileExistsError:
     pass
 
-shutil.copy("config.toml", path.join(root, "etc/camera.d", "config.toml"))
+shutil.copyfile("config.toml", path.join(root, "etc/camera.d", "config.toml"))
 
 try:
     mkdir(path.join(root, "etc/camera.d/presets"))
 except FileExistsError:
     pass
 
-shutil.copy("high.toml", path.join(root, "etc/camera.d/presets", "high.toml"))
+shutil.copyfile("high.toml", path.join(root, "etc/camera.d/presets", "high.toml"))
 
 for i in ["axp313a.lja", "axp313a.py"]:
-    shutil.copy(i, path.join(root, "bin", i))
+    shutil.copyfile(i, path.join(root, "bin", i))
 
-shutil.copy("axp313a.man", path.join(root, "usr/share/man", "axp313a.man"))
+shutil.copyfile("axp313a.man", path.join(root, "usr/share/man", "axp313a.man"))
